d91fff524bf8b0cf276e3a5de96c694dfc19d63d
carlos.leal <carloseduardoleal10@gmail.com>
2022-10-27 20:26:26 -0300

    Atualização

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import '../services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -10,23 +12,36 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  @override
-  void initState() {
-    super.initState();
-    getLocation();
-  }
+    @override
+    void initState() {
+       super.initState();
+       getLocation();
+    }
 
-  Future<void> getLocation() async {
+    void getData() async {
+      var url = Uri.parse('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+      http.Response response = await http.get(url);
+
+      if (response.statusCode == 200) { // se a requisição foi feita com sucesso
+        var data = response.body;
+        var jsonData = jsonDecode(data);
+        print(data);  // imprima o resultado
+      } else {
+        print(response.statusCode);  // senão, imprima o código de erro
+      }
+    }
+
+    Future<void> getLocation() async {
       Location location = Location();
       location.getCurrentLocation();
       print(location.latitude);
       print(location.longitude);
-  }
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+      getData();
+      return Scaffold(
+      );
+    }
 }
